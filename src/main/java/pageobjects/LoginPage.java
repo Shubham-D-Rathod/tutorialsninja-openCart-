98dd73b0ac3ce1cbd9995f20fbe6cd266b720524
Rathod <shubham-dayaram.rathod@capgemini.com>
2022-07-08 00:24:43 +0530

    commented

--- src/main/java/pageobjects/LoginPage.java
+++ src/main/java/pageobjects/LoginPage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
-	
+	// this is loginpage
 	WebDriver driver;
 	
 	public LoginPage(WebDriver driver) {
